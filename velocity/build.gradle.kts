7788d316841c7403b60a244beb6d4821adf21e21
Gero Lindner <gero.lindner@protonmail.com>
2024-07-30 14:09:04 +0200

    Remove standard gradle dependencies

--- velocity/build.gradle.kts
+++ velocity/build.gradle.kts
@@ -16,16 +16,7 @@ repositories {
 }
 
 dependencies {
-    // Use JUnit Jupiter for testing.
-    testImplementation(libs.junit.jupiter)
 
-    testRuntimeOnly("org.junit.platform:junit-platform-launcher")
-
-    // This dependency is exported to consumers, that is to say found on their compile classpath.
-    api(libs.commons.math3)
-
-    // This dependency is used internally, and not exposed to consumers on their own compile classpath.
-    implementation(libs.guava)
 }
 
 // Apply a specific Java toolchain to ease working on different environments.
