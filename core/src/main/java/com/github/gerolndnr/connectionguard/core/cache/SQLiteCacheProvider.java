811cfc009f73fd0bec92d12b0c42d648ad58109d
gerolndnr <gero.lindner@outlook.de>
2024-07-31 03:23:26 +0200

    Added addGeoResult and addVpnResult to SQLite cache provider

--- core/src/main/java/com/github/gerolndnr/connectionguard/core/cache/SQLiteCacheProvider.java
+++ core/src/main/java/com/github/gerolndnr/connectionguard/core/cache/SQLiteCacheProvider.java
@@ -4,10 +4,8 @@ import com.github.gerolndnr.connectionguard.core.ConnectionGuard;
 import com.github.gerolndnr.connectionguard.core.geo.GeoResult;
 import com.github.gerolndnr.connectionguard.core.vpn.VpnResult;
 
-import java.sql.Connection;
-import java.sql.DriverManager;
-import java.sql.SQLException;
-import java.sql.Statement;
+import java.sql.*;
+import java.util.Date;
 import java.util.Optional;
 import java.util.concurrent.CompletableFuture;
 
@@ -27,7 +25,9 @@ public class SQLiteCacheProvider implements CacheProvider {
 
                 Statement statement = connection.createStatement();
                 statement.execute("CREATE TABLE IF NOT EXISTS connectionguard_vpn_cache (address TEXT, vpn BOOLEAN, cached_on INTEGER);");
-                statement.execute("CREATE TABLE IF NOT EXISTS connectionguard_geo_cache (address TEXT, country_name TEXT, city_name TEXT, isp_name TEXT);");
+                statement.execute("CREATE TABLE IF NOT EXISTS connectionguard_geo_cache (address TEXT, country_name TEXT, city_name TEXT, isp_name TEXT, cached_on INTEGER);");
+                statement.execute("CREATE INDEX IF NOT EXISTS vpn_address ON connectionguard_vpn_cache (address)");
+                statement.execute("CREATE INDEX IF NOT EXISTS geo_address ON connectionguard_geo_cache (address)");
                 return true;
             } catch (SQLException e) {
                 ConnectionGuard.getLogger().info("SQLite | " + e.getMessage());
@@ -61,12 +61,42 @@ public class SQLiteCacheProvider implements CacheProvider {
 
     @Override
     public CompletableFuture<Void> addVpnResult(VpnResult vpnResult) {
-        return null;
+        return CompletableFuture.runAsync(() -> {
+            try {
+                PreparedStatement preparedStatement = connection.prepareStatement(
+                        "INSERT INTO connectionguard_vpn_cache (address, vpn, cached_on) VALUES (?, ?, ?)"
+                );
+
+                preparedStatement.setString(1, vpnResult.getIpAddress());
+                preparedStatement.setBoolean(2, vpnResult.isVpn());
+                preparedStatement.setLong(3, new Date().getTime());
+
+                preparedStatement.execute();
+            } catch (SQLException e) {
+                ConnectionGuard.getLogger().info("SQLite | " + e.getMessage());
+            }
+        });
     }
 
     @Override
     public CompletableFuture<Void> addGeoResult(GeoResult geoResult) {
-        return null;
+        return CompletableFuture.runAsync(() -> {
+            try {
+                PreparedStatement preparedStatement = connection.prepareStatement(
+                        "INSERT INTO connectionguard_geo_cache (address, country_name, city_name, isp_name, cached_on) VALUES (?, ?, ?, ?, ?)"
+                );
+
+                preparedStatement.setString(1, geoResult.getIpAddress());
+                preparedStatement.setString(2, geoResult.getCountryName());
+                preparedStatement.setString(3, geoResult.getCityName());
+                preparedStatement.setString(4, geoResult.getIspName());
+                preparedStatement.setLong(5, new Date().getTime());
+
+                preparedStatement.execute();
+            } catch (SQLException e) {
+                ConnectionGuard.getLogger().info("SQLite | " + e.getMessage());
+            }
+        });
     }
 
     @Override
