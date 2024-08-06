2b6ece150f7cfaadbb645d3110f3131544e8a850
Gero Lindner <gero.lindner@protonmail.com>
2024-08-06 15:45:36 +0200

    Add runPaper plugin to gradle

--- build.gradle.kts
+++ build.gradle.kts
@@ -3,6 +3,7 @@ import com.github.jengelman.gradle.plugins.shadow.tasks.ShadowJar
 plugins {
     `java-library`
     id("com.github.johnrengelman.shadow").version("8.1.1")
+    id("xyz.jpenilla.run-paper").version("2.3.0")
 }
 
 version = "0.1.0"
@@ -20,6 +21,17 @@ dependencies {
 }
 
 tasks.named<ShadowJar>("shadowJar") {
-    archiveVersion.set(project.version.toString())
-    relocate("com.alessiodp.libby", "com.github.gerolndnr.connectionguard.libs.com.alessiodp.libby")
+
+}
+
+tasks {
+    shadowJar {
+        archiveVersion.set(project.version.toString())
+        relocate("com.alessiodp.libby", "com.github.gerolndnr.connectionguard.libs.com.alessiodp.libby")
+    }
+
+    runServer {
+        jvmArgs("-Dcom.mojang.eula.agree=true")
+        minecraftVersion("1.8.8")
+    }
 }
